1259990832ec356433f62b06f547c5ef42ffe7d1
kuuhaku17 <116897004+kuuhaku17@users.noreply.github.com>
2023-12-20 00:29:24 +0700

    Add files via upload

---
+++ Seberapa sayang/asset/js/jquery.playSound.js
@@ -0,0 +1,16 @@
+
+(function ($) {
+    $.extend({
+        playSound: function () {
+            return $(
+                '<audio class="sound-player" autoplay="autoplay" style="display:none;">' +
+                '<source src="' + arguments[0] + '" />' +
+                '<embed src="' + arguments[0] + '" hidden="true" autostart="true" loop="true"/>' +
+                '</audio>'
+            ).appendTo('body');
+        },
+        stopSound: function () {
+            $(".sound-player").remove();
+        }
+    });
+})(jQuery);
